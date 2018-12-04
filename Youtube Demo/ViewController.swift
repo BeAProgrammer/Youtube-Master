e4762dca27e3ae60a3f7b2c3936f6174ef759b35
Mac <mac@macs-macbook-pro.local>
2018-12-04 12:02:09 +0530

    Testing

--- Youtube Demo/ViewController.swift
+++ Youtube Demo/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var youtubeView: YouTubePlayerView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadViewYoutube()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loadViewYoutube()
+    {
+        youtubeView.playerVars = ["playsinline": "1" as AnyObject,
+            "controls": "1" as AnyObject,
+            "showinfo": "0" as AnyObject]
+         youtubeView.loadPlaylistID("RDMMzZ2i_sb4xOg")
     }
-
-
+    @IBAction func playVdo(_ sender: UIButton)
+    {
+        
+      if  youtubeView.playerState.rawValue == "-1"
+      {
+        youtubeView.play()
+        sender.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+      }
+        else if youtubeView.playerState.rawValue == "2"
+      {
+        youtubeView.pause()
+        sender.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+      }
+        else if youtubeView.playerState.rawValue == "1"
+      {
+        youtubeView.play()
+        sender.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        }
+        
+        
+        
+    }
+    
+    @IBAction func nextVdo(_ sender: Any)
+    {
+      youtubeView.nextVideo()
+    }
+    
+    @IBAction func preVdo(_ sender: Any)
+    {
+      youtubeView.previousVideo()
+    }
+    
+    
 }
 
