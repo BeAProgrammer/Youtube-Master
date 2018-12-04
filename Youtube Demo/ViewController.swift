7781d03870f591fc1a44d02c74ea54a9c457f57b
admin <admin@admins-macbook-pro.local>
2018-12-04 15:35:11 +0530

    print.

--- Youtube Demo/ViewController.swift
+++ Youtube Demo/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loadViewYoutube()
         print("Done")
+        print("ReDone")
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     func loadViewYoutube()
